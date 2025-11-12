01ce6d1dff1485d770b9ebd665d6d7e5e754957f
S HARI SHANKAR <hari.240123@cse.ritchennai.edu.in>
2025-11-12 18:28:05 +0530

    Add files via upload

---
+++ UNIT 2/try_catch_finally.java
@@ -0,0 +1,16 @@
+package excepetion_handling;
+public class try_catch_finally {
+		  public static void main(String[] args) {
+		    try {
+		      int[] myNumbers = {1, 2, 3};
+		      System.out.println(myNumbers[10]);
+		    } catch (Exception e) {
+		      System.out.println("Something went wrong.");
+		    } finally {
+		      System.out.println("The 'try catch' is finished.");
+		    }
+		  }
+		}
+
+
+
