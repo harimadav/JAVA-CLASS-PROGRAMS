ece5c265a177c5d491e9dfc0bc32175f8f034146
S HARI SHANKAR <hari.240123@cse.ritchennai.edu.in>
2025-11-19 21:35:50 +0530

    Add files via upload

---
+++ UNIT 2/file.java
@@ -0,0 +1,23 @@
+package file;
+import java.io.File;  // Import the File class
+import java.io.IOException;  // Import the IOException class to handle errors
+public class file {
+	  public static void main(String[] args) {
+	    try {
+	      File myObj = new File("one.txt");
+	      if (myObj.createNewFile()) {
+	        System.out.println("File created: " + myObj.getName());
+	      } else {
+	        System.out.println("File already exists.");
+	      }
+	    } catch (IOException e) {
+	      System.out.println("An error occurred.");
+	      e.printStackTrace();
+	    }
+	  }
+	
+
+
+	  
+
+}
