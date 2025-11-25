f5438f08a58dbc634c7d487d6689e596c5cd4f22
S HARI SHANKAR <hari.240123@cse.ritchennai.edu.in>
2025-11-25 21:08:37 +0530

    Add files via upload

---
+++ UNIT 2/final_keyword.java
@@ -0,0 +1,16 @@
+package keywrods;
+
+
+public class final_keyword {
+	final public void fun() {
+		System.out.println("RIT");
+	}
+	public static void main(String[] args) {
+		final_keyword O =new final_keyword();
+		O.fun();
+	}
+}
+	
+		
+
+
