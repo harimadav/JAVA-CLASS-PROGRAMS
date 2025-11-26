4dd256ecebf89b72953f593c68b061acd42d29e2
S HARI SHANKAR <hari.240123@cse.ritchennai.edu.in>
2025-11-26 21:23:35 +0530

    Add files via upload

---
+++ UNIT 2/fun1.java
@@ -0,0 +1,16 @@
+package keywrods;
+
+
+public class fun1 {
+	public static void main(String[] args) {
+		final int a=1;
+		a=5;// final is the keyword which is assined to the variable then the var value can't be changed
+		System.out.println(a);
+
+		
+		
+	}
+	
+}
+
+
