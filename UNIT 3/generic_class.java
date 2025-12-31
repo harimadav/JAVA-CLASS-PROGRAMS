73d14e4c90feb22f6a703d12bf2dafa31732e463
S HARI SHANKAR <hari.240123@cse.ritchennai.edu.in>
2025-12-31 20:45:13 +0530

    Add files via upload

---
+++ UNIT 3/generic_class.java
@@ -0,0 +1,8 @@
+
+	
+	// like T there are U,V,E these are paramters
+	//mulitporcessing more than CPU(hardware)
+	//multiprogramming -RAM initalyy from HA and VAH sw(hw)  sw-software ,hw -hardware
+	//mulittasking - doing more tasks speedly sw
+	//multithreading parrely the programs works
+	
