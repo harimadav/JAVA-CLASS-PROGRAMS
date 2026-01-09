8a43a5b39280b2b9c97f80f027d0a9604709d087
S HARI SHANKAR <hari.240123@cse.ritchennai.edu.in>
2026-01-09 21:29:22 +0530

    Update day74.java
    Remove the sumOddLengthSubarrays method from the Solution class.

--- UNIT 3/day74.java
+++ UNIT 3/day74.java
@@ -1,14 +1,1 @@
-class Solution {
-    public int sumOddLengthSubarrays(int[] arr) {
-        int totalsum=0;
-        for(int i =0;i<arr.length;i++){
-            int left =i+1;
-            int right = arr.length -i;
-            int totalsubarrays=left*right;
-            int odds=(totalsubarrays+1)/2;
-            int cont = arr[i]* odds;
-            totalsum+=cont;
-        }
-        return totalsum;
-    }
-}
+
