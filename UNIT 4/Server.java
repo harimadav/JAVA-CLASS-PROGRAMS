72c87cd715488296143667ee74fdda85bd9a4ca2
S HARI SHANKAR <hari.240123@cse.ritchennai.edu.in>
2026-01-27 21:18:05 +0530

    Add files via upload

---
+++ UNIT 4/Server.java
@@ -0,0 +1,17 @@
+package RMI_PROCESS;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+public class Server {
+    public static void main(String[] args) {
+        try {
+            HelloImpl obj = new HelloImpl();
+            Registry registry = LocateRegistry.createRegistry(1099); // Default RMI port
+            registry.rebind("HelloService", obj);
+            System.out.println("Server ready.");
+        } catch (Exception e) {
+            System.err.println("Server exception: " + e.toString());
+            e.printStackTrace();
+        }
+    }
+}
